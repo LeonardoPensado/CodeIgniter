766427e817c75f4948757f45962e73b56a495fea
LeoonardoPensado <leoopensado94@gmail.com>
2021-12-20 17:26:13 -0600

    a

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,2 +1,15 @@
-<?php
-    echo "Hello World";
+<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<meta name="google" content="notranslate" />
+			<meta http-equiv="Content-Language" content="es_MX" />
+			<title>Leonardo David Pensado Hernández</title>
+		</head>
+        
+        <body>
+            
+        </body>
+
+		
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,8 +1,13 @@
 <html>
 <head> 
-    <title>CodeIgniter Tutorial</title>
+    <title>Leonardo David Pensado Hernández</title>
 </head>
 <body>
-
+<nav>
+    <li><a href="#">Bienvenidos</a></li>
+    <li><a href="#">Conocimientos</a></li>
+    <li><a href="#">Escuela</a></li>
+    <li><a href="#">Contacto</a></li>
+</nav>    
 </body>
 </html>
